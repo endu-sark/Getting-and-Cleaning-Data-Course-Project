722aa593d3db6e4ed56b6a838ea7560b036ab378
Sirsendu <22126669+endu-sark@users.noreply.github.com>
2018-08-29 21:59:28 +0530

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,54 @@
+#getting data
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="F:/one drive/R/data.zip")
+unzip(zipfile="F:/one drive/R/data.zip")
+setwd("F:/one drive/R/UCI HAR Dataset")
+#including required libraries
+library(plyr)
+library(data.table)
+#loading data
+subjectTrain = read.table('./train/subject_train.txt',header=FALSE)
+xTrain = read.table('./train/x_train.txt',header=FALSE)
+yTrain = read.table('./train/y_train.txt',header=FALSE)
+subjectTest = read.table('./test/subject_test.txt',header=FALSE)
+xTest = read.table('./test/x_test.txt',header=FALSE)
+yTest = read.table('./test/y_test.txt',header=FALSE)
+#combining data sets into single file
+xDataSet <- rbind(xTrain, xTest)
+yDataSet <- rbind(yTrain, yTest)
+subjectDataSet <- rbind(subjectTrain, subjectTest)
+dim(xDataSet)
+dim(yDataSet)
+dim(subjectDataSet)
+#subsetting on mean and sd
+xDataSet_mean_std <- xDataSet[, grep("-(mean|std)\\(\\)", read.table("features.txt")[, 2])]
+names(xDataSet_mean_std) <- read.table("features.txt")[grep("-(mean|std)\\(\\)", read.table("features.txt")[, 2]), 2] 
+View(xDataSet_mean_std)
+dim(xDataSet_mean_std)
+#naming datasets
+yDataSet[, 1] <- read.table("activity_labels.txt")[yDataSet[, 1], 2]
+names(yDataSet) <- "Activity"
+View(yDataSet)
+names(subjectDataSet) <- "Subject"
+#labelling the data columns
+summary(subjectDataSet)
+singleDataSet <- cbind(xDataSet_mean_std, yDataSet, subjectDataSet)
+names(singleDataSet) <- make.names(names(singleDataSet))
+names(singleDataSet) <- gsub('Acc',"Acceleration",names(singleDataSet))
+names(singleDataSet) <- gsub('GyroJerk',"AngularAcceleration",names(singleDataSet))
+names(singleDataSet) <- gsub('Gyro',"AngularSpeed",names(singleDataSet))
+names(singleDataSet) <- gsub('Mag',"Magnitude",names(singleDataSet))
+names(singleDataSet) <- gsub('^t',"TimeDomain.",names(singleDataSet))
+names(singleDataSet) <- gsub('^f',"FrequencyDomain.",names(singleDataSet))
+names(singleDataSet) <- gsub('\\.mean',".Mean",names(singleDataSet))
+names(singleDataSet) <- gsub('\\.std',".StandardDeviation",names(singleDataSet))
+names(singleDataSet) <- gsub('Freq\\.',"Frequency.",names(singleDataSet))
+names(singleDataSet) <- gsub('Freq$',"Frequency",names(singleDataSet))
+View(singleDataSet)
+names(singleDataSet)
+#creating another tidy data set
+Data2<-aggregate(. ~Subject + Activity, singleDataSet, mean)
+Data2<-Data2[order(Data2$Subject,Data2$Activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+View(Data2)
+write.table(Data2, "tidy_data_set.txt", row.names = FALSE, quote = FALSE)
